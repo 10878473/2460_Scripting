ffdcdcd43fed1c5e5476394500351cda7f5df72d
Joseph <10878473@uvu.edu>
2024-09-09 14:02:10 -0600

    Added Behavior script from previous assignments

---
+++ Unit 3/Lab-3-Maching/Assets/EventTrigger.cs
@@ -0,0 +1,38 @@
+
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventTrigger : MonoBehaviour
+{
+    public UnityEvent TriggerEnterEvent;
+    public UnityEvent TriggerExitEvent;
+
+    public UnityEvent StartEvent;
+    public UnityEvent awakeEvent;
+    //when the gameobject collides with another gameobject - trigger something from a scriptableobject
+    private void Start()
+    {
+        StartEvent.Invoke();
+    }
+
+    private void Awake()
+    {
+        awakeEvent.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Triggering event, gameobject "+ gameObject.name + "triggered from collider "+ other.gameObject.name);
+        TriggerEnterEvent.Invoke();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        TriggerExitEvent.Invoke();
+    }
+    /*private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(gameObject.name + "collided with collision "+ other.gameObject.name);
+        TriggerEnterEvent.Invoke();
+    }*/
+}
