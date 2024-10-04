6e891b7b3d540a08dea1aeb32ceb8f8c966f4236
Desktop <10878473@uvu.edu>
2024-10-03 21:36:41 -0600

    Started Whiteboxing Unity

---
+++ Platformer Project/Assets/Scripts From Matching game/CoroutineBehavior.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class CoroutineBehavior : MonoBehaviour
+{
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalseEvent;
+    public IntData counterNum;
+    public bool _canRun = true;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wfuObj;
+
+    public bool CanRun
+    {
+        get => _canRun;
+        set => _canRun = value;
+    }
+
+    private void Start()
+    {
+        startEvent.Invoke();
+        wfsObj = new WaitForSeconds(seconds);
+
+    }
+
+    public void StartCounting()
+    {
+        StartCoroutine(Counting());
+    }
+    private IEnumerator Counting()
+    {
+        wfuObj = new WaitForFixedUpdate();
+        startCountEvent.Invoke();
+        Debug.Log("Start from Coroutine script ran");
+        yield return wfsObj;
+
+        while (counterNum.value > 0)
+        {
+            repeatCountEvent.Invoke();
+            counterNum.value--;
+            yield return wfsObj;
+            Debug.Log("run on start");
+        }
+        endCountEvent.Invoke();
+    }
+
+    public void StartRepeatUntilFalse()
+    {
+        CanRun = true;
+        StartCoroutine(RepeatUntilFalse());
+    }
+
+    private IEnumerator RepeatUntilFalse()
+    {
+        yield return wfsObj;
+        while (CanRun)
+        {
+            
+            repeatUntilFalseEvent.Invoke();
+            yield return wfsObj;
+        }
+    }
+    
+}
