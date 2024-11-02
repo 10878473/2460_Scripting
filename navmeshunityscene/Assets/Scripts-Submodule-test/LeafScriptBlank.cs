b64210ccd5ad5aae5f41dbe069a5a959fc5b556e
Joseph <10878473@uvu.edu>
2024-11-01 23:06:17 -0600

    Unity navmesh challenge complete
    i just pushed the wrong repository, today has been a mess. Unity is not working, cannot open any projects, life sucks

---
+++ navmeshunityscene/Assets/Scripts-Submodule-test/LeafScriptBlank.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafScriptBlank : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(
+            "Hello leafy world!"
+        );
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
