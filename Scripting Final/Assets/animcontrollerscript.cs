8eb480cfc9328e768dafe01cda1e50a29b94babe
Joseph <10878473@uvu.edu>
2024-12-05 20:13:56 -0700

    animation state machine

--- Scripting Final/Assets/animcontrollerscript.cs
+++ Scripting Final/Assets/animcontrollerscript.cs
@@ -1,21 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.TextCore.Text;
 
 public class animcontrollerscript : MonoBehaviour
 {
+    
+    //   USED VARIABLES
     public UnityEvent Run, Idle, runshoot, jump, land, flyshoot, idleshoot, cantshoot;
 
-    public bool running, airborn, flying, canshoot, idling,wasairborn;
+    public bool running, airborn, flying, canshoot, idling,wasairborn, isGrounded;
     public CharacterController controller;
     //private float inputy;
     private float inputfire;
     private float inputx;
     private float inputJump;
+    private Vector3 rayStart;
     
     public PlayerStats stats;
+    public LayerMask groundMask; // Specify what layers are considered ground
+
+    //DEBUG VARIABLES
+    public TMP_Text grounded, isrunning, idle, wasinair;
+    //
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -25,12 +36,34 @@ public class animcontrollerscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        TakeInput();
+        ChangeState();
+        DisplayDebug();
+    }
+    void TakeInput()
+    {
         inputx = Input.GetAxis("Horizontal");
         //inputy = Input.GetAxis("Vertical");
         inputfire = Input.GetAxis("Fire1"); 
         inputJump = Input.GetAxis("Jump");
         airborn = !controller.isGrounded;
-        if (!airborn && inputx != 0)
+        // Define the point slightly below the character
+        rayStart = controller.transform.position + Vector3.down * (controller.height/2 + .1f);
+        // Raycast downwards to check for ground
+        isGrounded = Physics.Raycast(rayStart, Vector3.down, .5f, groundMask);
+        
+    }
+
+    void DisplayDebug()
+    {
+        Debug.DrawRay(rayStart, Vector3.down * .5f, isGrounded ? Color.green : Color.red);
+        Debug.Log("Is Grounded: " + isGrounded);
+        
+    }
+
+    void ChangeState()
+    {
+        if (isGrounded && inputx != 0)// if on the ground and movement keys are pressed, run.
         {
             if (!running)
             {
@@ -46,20 +79,36 @@ public class animcontrollerscript : MonoBehaviour
                 running = false;
                 idling = true;
                 Debug.Log("idling");
+                
                 Idle.Invoke();
             }
         }
 
-        if (!airborn && inputJump >0)
+        /*if (isGrounded && inputJump !=0 && (running || idling))
         {
-            airborn = true;
-            wasairborn = true;
-            jump.Invoke();
+            // If player was on the ground, then jumps, do jump anim. 
+            if (!wasairborn)
+            {
+                wasairborn = true;
+                jump.Invoke();
+            }
             
+        }*/
+        
+        if (inputJump !=0 && stats.isflying &&(running || idling))
+        {
+            // If player was on the ground, then jumps, do jump anim.
+            if (!wasairborn)
+            {
+                running = false;
+                idling = false;
+                wasairborn = true;
+                jump.Invoke();
+            }
 
         }
 
-        if (!airborn && wasairborn)
+        if (isGrounded && wasairborn)
         {
             wasairborn = false;
             land.Invoke();
